730c60d1c00ed3a4eaed63fedf146d2f06a9a61c
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-04-06 13:44:12 -0600

    Challenge 3

--- test/octalConverterTest.js
+++ test/octalConverterTest.js
@@ -16,10 +16,24 @@ describe('givenDecimal1WhenConvertThenOctal1', function(){
 })
 
 describe('givenDecimal2WhenConvertThenOctal2', function(){
-    it('convert(1) = 1', function() {
+    it('convert(2) = 2', function() {
         //GIVEN 
         var a = 2;
         var expectedResult = 2;
+
+        //WHEN 
+        var actualResult = octalConverter.convert(a);
+        
+        //THEN 
+        assert.strictEqual(expectedResult, actualResult);
+    })
+})
+
+describe('givenDecimal8WhenConvertThenOctal10', function(){
+    it('convert(8) = 10', function() {
+        //GIVEN 
+        var a = 8;
+        var expectedResult = 10;
 
         //WHEN 
         var actualResult = octalConverter.convert(a);
